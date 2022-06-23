3609f11012fe1d95d825b44d5cca411af838aa86
Aarush Popli <popliaarush00@gmail.com>
2022-06-23 16:17:27 +0530

    contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Contact.module.css'
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const data = { name, email, desc };
+
+    fetch('http://localhost:3000/api/postcontact/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.text())
+      .then(data => {
+        console.log('Success:', data);
+        alert("Thanks for your message");
+        setName('');
+        setEmail('');
+        setDesc('');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  const handleChange = (e) => {
+    if (e.target.name === 'name') {
+      setName(e.target.value);
+    }
+    else if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    }
+    else if (e.target.name === 'desc') {
+      setDesc(e.target.value);
+    }
+  }
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [desc, setDesc] = useState('');
   return (
     <>
       <main className={styles.main}>
         <div className={styles.body}>
           <h2>Contact Us</h2>
-          <form>
-            <div style={{marginBottom : "1rem"}}>
-              <label for="name" class={styles.label}>Name</label>
-              <input type="text" class={styles.control} id="name" placeholder="Enter your name" />
+          <form onSubmit={handleSubmit}>
+            <div style={{ marginBottom: "1rem" }}>
+              <label htmlFor="name" className={styles.label}>Name</label>
+              <input type="text" className={styles.control} id="name" value={name} onChange={handleChange} name='name' placeholder="Enter your name" required />
             </div>
-            <div style={{marginBottom : "1rem"}}>
-              <label for="email" class={styles.label}>Email address</label>
-              <input type="email" class={styles.control} id="exampleFormControlInput2" placeholder="Enter your email" />
+            <div style={{ marginBottom: "1rem" }}>
+              <label htmlFor="email" className={styles.label}>Email address</label>
+              <input type="email" className={styles.control} id="email" value={email} onChange={handleChange} name='email' placeholder="Enter your email" required />
             </div>
-            <div style={{marginBottom : "1rem"}}>
-              <label for="desc" class={styles.label}>Message</label>
-              <textarea class={styles.control} id="desc" rows="5" placeholder='Enter your message'></textarea>
+            <div style={{ marginBottom: "1rem" }}>
+              <label htmlFor="desc" className={styles.label}>Message</label>
+              <textarea className={styles.control} id="desc" value={desc} onChange={handleChange} name='desc' rows="5" placeholder='Enter your message' required></textarea>
             </div>
-            <button type="submit" class={styles.button}>Send</button>
+            <button type="submit" className={styles.button}>Send</button>
           </form>
         </div>
       </main>
